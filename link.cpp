891140a8e23829cd9fc2b794a4c9ace45f432f3e
Continue <1006480577@qq.com>
2019-05-24 13:19:07 +0800

    封装了Link类简化实现网络连接

---
+++ link.cpp
@@ -0,0 +1,66 @@
+#include <QString>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include "link.h"
+
+
+Link::Link(QString Url, QString parameter)
+    : Url(Url),
+      parameter(parameter)
+{
+    manage = new QNetworkAccessManager();
+}
+
+Link::~Link()
+{
+    delete manage;
+}
+void Link::setUrl(QString Url)
+{
+    this->Url = Url;
+}
+
+void Link::setParameter(QString formalParam, QString actualParam)
+{
+
+    this->parameter = (formalParam + "=" + actualParam);
+}
+
+QString Link::getUrl() const
+{
+    return Url;
+}
+
+QString Link::getParameter() const
+{
+    return parameter;
+}
+
+void Link::addParameter(QString formalParam, QString actualParam)
+{
+    QString parameter = (formalParam + "=" + actualParam);
+    this->parameter += "&"+parameter;
+}
+
+void Link::bindSlot(const QObject *receiver, const char *member) const
+{
+    QObject::connect(manage, SIGNAL(finished(QNetworkReply*)), receiver, member);
+}
+
+void Link::post() const
+{
+    QByteArray content = parameter.toUtf8();
+    QNetworkRequest netReq;
+    netReq.setUrl(QUrl(Url));
+    netReq.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+    manage->post(netReq, content);
+}
+
+QString Link::getReply(QNetworkReply *reply)
+{
+    QString result = reply->readAll();
+    return result;
+}
+
+
+
---
+++ link.h
@@ -0,0 +1,32 @@
+#ifndef LINK_H
+#define LINK_H
+
+#include <QString>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+
+class Link : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Link(QString Url = "", QString parameter = "");
+    ~Link();
+
+    void setUrl(QString Url);
+    void setParameter(QString formalParam, QString actualParam);//设置参数(前面是形参,后面是实参)
+    QString getUrl() const;
+    QString getParameter() const;
+    void addParameter(QString formalParam, QString actualParam);//添加参数(前面是形参,后面是实参)
+
+    void bindSlot(const QObject *receiver, const char *member) const;//当得到回应信息时绑定槽函数
+
+    void post() const;//利用POST方式发送请求
+
+    static QString getReply(QNetworkReply*);//获取回应信息(QString),这是static函数,可以不创建对象直接用
+private:
+    QString Url;//网络ip地址
+    QString parameter;//发送的参数
+    QNetworkAccessManager *manage;
+};
+
+#endif // LINK_H
