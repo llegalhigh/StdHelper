fdd08b0e1a12fa069b3c7f65ad2b6099a51f1ce3
Continue <1006480577@qq.com>
2019-05-27 15:20:21 +0800

    完善登陆注册页面

--- Login.cpp
+++ Login.cpp
@@ -10,7 +10,7 @@ Login::Login(QWidget *parent) :
     link(new Link)
 {
     ui->setupUi(this);
-    init();
+    loginInit();
 }
 
 Login::~Login()
@@ -19,14 +19,55 @@ Login::~Login()
     delete link;
 }
 
-void Login::init()
+void Login::loginSetTabOrder() const
+{
+    setTabOrder(ui->lineEdit_username, ui->lineEdit_password);
+    setTabOrder(ui->lineEdit_password, ui->pushButton_login);
+    setTabOrder(ui->pushButton_login, ui->pushButton_register);
+    ui->lineEdit_username->setFocus();
+}
+
+void Login::registerSetTabOrder() const
+{
+    setTabOrder(ui->lineEdit_username, ui->lineEdit_password);
+    setTabOrder(ui->lineEdit_password, ui->lineEdit_confirmpwd);
+    setTabOrder(ui->lineEdit_confirmpwd, ui->pushButton_login);
+    setTabOrder(ui->pushButton_login, ui->pushButton_register);
+    ui->lineEdit_username->setFocus();
+}
+
+void Login::loginInit() const
 {
     ui->label_screen->setText(tr("Login Screen"));
+    ui->lineEdit_username->setText(tr(""));
+    ui->lineEdit_password->setText(tr(""));
+    ui->lineEdit_confirmpwd->setText(tr(""));
     ui->label_confirmpwd->hide();
     ui->lineEdit_confirmpwd->hide();
 
+    ui->pushButton_login->disconnect();
+    ui->pushButton_register->disconnect();
     connect(ui->pushButton_login, SIGNAL(clicked()), this, SLOT(loginSend()));
-    connect(ui->pushButton_register, SIGNAL(clicked()), this, SLOT(changeToRegisterScreen()));
+    connect(ui->pushButton_register, SIGNAL(clicked()), this, SLOT(registerInit()));
+
+    loginSetTabOrder();
+}
+
+void Login::registerInit() const
+{
+    ui->label_screen->setText(tr("Register Screen"));
+    ui->lineEdit_username->setText(tr(""));
+    ui->lineEdit_password->setText(tr(""));
+    ui->lineEdit_confirmpwd->setText(tr(""));
+    ui->label_confirmpwd->show();
+    ui->lineEdit_confirmpwd->show();
+
+    ui->pushButton_login->disconnect();
+    ui->pushButton_register->disconnect();
+    connect(ui->pushButton_register, SIGNAL(clicked()), this, SLOT(registerSend()));
+    connect(ui->pushButton_login, SIGNAL(clicked()), this, SLOT(loginInit()));
+
+    registerSetTabOrder();
 }
 
 void Login::paintEvent(QPaintEvent *event)
@@ -37,21 +78,21 @@ void Login::paintEvent(QPaintEvent *event)
     style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
 }
 
-void Login::loginSend()
+void Login::loginSend() const
 {
     ui->label_information->setText(QObject::tr("Logining...")); //正在登陆时显示"Logining..."信息
 
     QString username = ui->lineEdit_username->text();
     QString password = ui->lineEdit_password->text();
 
-    link->setUrl("http://111.230.183.100/login.php");   //登陆ip地址(示例)
+    link->setUrl("http://111.230.183.100/demo.php");   //登陆ip地址(示例)
     link->setParameter("username",username);    //设置参数
     link->addParameter("password",password);    //添加参数
     link->connect(this,SLOT(cancelInformation()));  //绑定槽函数(网络请求完成时触发槽函数)
     link->post();   //发送POST请求
 }
 
-void Login::registerSend()
+void Login::registerSend() const
 {
     ui->label_information->setText(QObject::tr("Registering...")); //正在登陆时显示"Registering..."信息
 
@@ -68,25 +109,7 @@ void Login::registerSend()
 
 }
 
-void Login::changeToLoginScreen()
-{
-    ui->pushButton_login->disconnect();
-    ui->pushButton_register->disconnect();
-    init();
-}
-
-void Login::changeToRegisterScreen()
-{
-    ui->label_screen->setText(tr("Register Screen"));
-    ui->label_confirmpwd->show();
-    ui->lineEdit_confirmpwd->show();
-
-    ui->pushButton_login->disconnect();
-    connect(ui->pushButton_register, SIGNAL(clicked()), this, SLOT(registerSend()));
-    connect(ui->pushButton_login, SIGNAL(clicked()), this, SLOT(changeToLoginScreen()));
-}
-
-void Login::cancelInformation()
+void Login::cancelInformation() const
 {
     ui->label_information->setText("");
     link->disconnect();
--- Login.h
+++ Login.h
@@ -17,17 +17,18 @@ class Login : public QWidget
 public:
     explicit Login(QWidget *parent = nullptr);
     ~Login();
-    void init();
+    void loginSetTabOrder() const;	//对登陆页面设置Tab的顺序, 并初始化焦点
+    void registerSetTabOrder() const;	//对注册页面设置Tab的顺序, 并初始化焦点
 
 protected:
     void paintEvent(QPaintEvent *); //重构QWidget函数, 使其能够显示背景图片
 
 private slots:
-    void loginSend(); //login按键clicked事件,发送登陆请求
-    void registerSend();    //register按键clicked事件,发送注册请求
-    void changeToLoginScreen(); //切换到登陆页面
-    void changeToRegisterScreen();  //切换到注册页面
-    void cancelInformation();   //当得到网络回应时取消显示的"Logining..."或"Registering..."信息
+    void loginSend() const; //login按键clicked事件,发送登陆请求
+    void registerSend() const;    //register按键clicked事件,发送注册请求
+    void cancelInformation() const;   //当得到网络回应时取消显示的"Logining..."或"Registering..."信息
+    void loginInit() const;   //初始化成登陆页面
+    void registerInit() const;    //初始化成注册页面
 
 private:
     Ui::Login *ui;
